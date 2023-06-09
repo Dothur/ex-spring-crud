0cb2baf24e85f2c114678419972c4df3de77f257
dothur <craz1000@naver.com>
2023-06-09 18:25:09 +0900

    :sparkles: feat. read one & Update & Delete - crud 구현 완성.. - 복습하기

--- src/main/java/com/example/crud/StudentController.java
+++ src/main/java/com/example/crud/StudentController.java
@@ -4,6 +4,7 @@ import com.example.crud.model.StudentDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -35,16 +36,72 @@ public class StudentController {
         // 데이터를 전송을 하고 새로고침 했을때 post 요청이 다시 중복해서
         // 발생 할 수 있기 때문에 redirect 를 사용해서
         // 데이터 처리가 잘 되었으니 다른 곳(url)으로 이동하게 하기 (Get 으로)
-//        return "redirect:/create-view";
+//        return "/create-view";
         return "redirect:/home";
     }
 
     @GetMapping("/home")
-    public String read(Model model){
+    public String home(Model model) {
         model.addAttribute(
                 "studentList",
                 studentService.readStudentAll()
         );
         return "home";
+    }
+
+    @GetMapping("/{id}")
+    public String read(
+            @PathVariable("id") Long id,
+            Model model
+    ) {
+        model.addAttribute(
+                "student",
+                studentService.readStudent(id)
+        );
+        return "read";
+    }
+
+    @GetMapping("/{id}/update-view")
+    public String updateView(
+            @PathVariable("id") Long id,
+            Model model
+    ) {
+        model.addAttribute(
+                "student",
+                studentService.readStudent(id)
+        );
+        return "update";
+    }
+
+    @PostMapping("/{id}/update")
+    public String update(
+            @PathVariable("id") Long id,
+            @RequestParam("name") String name,
+            @RequestParam("email") String email
+    ) {
+        // service 활용하기
+        studentService.updateStudent(id, name, email);
+        return "redirect:/" + id;
+    }
+
+    @GetMapping("/{id}/delete-view")
+    public String deleteView(
+            @PathVariable("id") Long id,
+            Model model
+    ) {
+        StudentDto studentDto = studentService.readStudent(id);
+        model.addAttribute("student", studentDto);
+        return "delete";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String delete(
+            @PathVariable("id") Long id
+    ) {
+        studentService.deleteStudent(id);
+        // update 때는 데이터가 남아있지만
+        // delete 는 돌아갈 상세보기가 없다
+        // 그래서 홈으로 돌아간다.
+        return "redirect:/home";
     }
 }
--- src/main/java/com/example/crud/StudentService.java
+++ src/main/java/com/example/crud/StudentService.java
@@ -12,16 +12,55 @@ public class StudentService {
     private final List<StudentDto> studentList = new ArrayList<>();
     private long nextId = 1L;
 
+    public StudentService(){
+        createStudent("alex", "alex@gmail.com");
+        createStudent("brad", "brad@gmail.com");
+        createStudent("chad", "chad@gmail.com");
+    }
+
     // 새로운 StudentDto 를 생성하는 메소드
-    public StudentDto createStudent(String name, String email){
-        StudentDto newStudent = new StudentDto(
-                nextId, name, email
-        );
+    public StudentDto createStudent(String name, String email) {
+        StudentDto newStudent = new StudentDto(nextId, name, email);
         nextId++;
         studentList.add(newStudent);
         return newStudent;
     }
+
+    // Service 에서 모든 StudentDto 를 주는 메소드
     public List<StudentDto> readStudentAll() {
         return studentList;
+    }
+
+    // Service 에서 단일 StudentDto 를 주는 메소드를 만들기
+    public StudentDto readStudent(Long id) {
+        // TODO
+        for (StudentDto studentDto : studentList) {
+            if (studentDto.getId().equals(id)){
+                return studentDto;
+            }
+        }
+        return null;
+    }
+    public StudentDto updateStudent(Long id, String name, String email){
+        StudentDto targetDto = this.readStudent(id);
+        if (targetDto != null){
+            targetDto.setName(name);
+            targetDto.setEmail(email);
+            return targetDto;
+        } else return null;
+    }
+
+    public boolean deleteStudent(Long id){
+        int target = -1;
+        for (int i = 0; i < studentList.size(); i++) {
+            if (studentList.get(i).getId().equals(id)) {
+                target = i;
+                break;
+            }
+        }
+        if (target != -1){
+            studentList.remove(target);
+            return true;
+        } else return false;
     }
 }
